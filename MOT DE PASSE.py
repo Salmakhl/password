59971df8c20d31ccf9a74bd32510bbbbf87f3624
Salmakhl <147110378+Salmakhl@users.noreply.github.com>
2023-10-23 22:28:44 +0100

    Add files via upload

---
+++ MOT DE PASSE.py
@@ -0,0 +1,19 @@
+pw=input("Enter the password: ")
+i = 1
+while pw != "DEV@2023" and i < 3:
+        pw= input("incorrect ,please try again: ")
+        i += 1
+if pw== "DEV@2023":
+    print("correct, HELLO 😊")
+
+if i == 3 and pw!= "DEV@2023"  :
+    x = input("What's your favorite film? ")
+
+
+
+
+    
+
+    
+    
+    
